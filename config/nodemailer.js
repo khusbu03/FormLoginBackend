099e4410c1e2d9c3070e7f2f6cc60f6dcf2a0510
khushbu <kphulara@innow8apps.com>
2025-03-11 17:30:53 +0530

    updated env

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,12 +5,12 @@ const ApiResponse = require("../utils/apiResponse");
 async function sendEmail(mailOptions) {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp-relay.brevo.com",
+      host: process.env.SMTP_HOST,
       port: 587,
       secure: false,
       auth: {
-        user: "87a05f002@smtp-brevo.com",
-        pass: "xsmtpsib-eebf19f06657ba5d47a4cd9cad8a68f6ac02b9becafe28260cafb77aeabb2670-VQJ8kpFKmGW7SxvC"
+        user: process.env.SMTP_KEY_NAME,
+        pass: process.env.SMTP_KEY_VALUE
       }
     });
 
